fdece12b1d4c85e343e43d65658b8ffd6cd931ee
Morteza <alirezainnaji@gmail.com>
2021-09-13 13:57:07 +0430

    build post

--- src/components/posts/Post/Post.js
+++ src/components/posts/Post/Post.js
@@ -1,9 +1,23 @@
 import React from 'react'
 
-function Post() {
+function Post(props) {
+    postdate = new Date(props.date)
     return (
         <div>
-            
+            <div className="Post_image">
+                <image src={props.image} />
+            </div>
+            <div className="Post_container">
+                <h3>
+                    <a href={props.link}>{props.title}</a>
+                </h3>
+                <p>{props.news_text}</p>
+            </div>
+            <div className="Post_footer">
+                {publish_date}
+            </div>
+
+
         </div>
     )
 }
