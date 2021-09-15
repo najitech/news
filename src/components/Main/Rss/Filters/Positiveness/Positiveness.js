db5bca1e257e610d7fd032fcd00c2bbda9142c93
Morteza <alirezainnaji@gmail.com>
2021-09-15 16:33:53 +0430

    design positiveness

--- src/components/Main/Rss/Filters/Positiveness/Positiveness.js
+++ src/components/Main/Rss/Filters/Positiveness/Positiveness.js
@@ -1,22 +1,45 @@
 import React from 'react'
 import './Positiveness.css'
-import Radio from '@material-ui/core/Radio';
-import RadioGroup from '@material-ui/core/RadioGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormControl from '@material-ui/core/FormControl';
-import FormLabel from '@material-ui/core/FormLabel';
+import { makeStyles } from '@material-ui/core/styles';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import RestoreIcon from '@material-ui/icons/Restore';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import RemoveCircleIcon from '@material-ui/icons/RemoveCircle';
+import {FaBalanceScale} from 'react-icons/fa';
+import {BsCircle} from 'react-icons/bs';
+import RadioButtonCheckedIcon from '@material-ui/icons/RadioButtonChecked';
+const useStyles = makeStyles({
+  root: {
+    width: 500,
+    "&$selected": {
+      color: "white"
+    }
+  },
+  selected: {
+    color: "white"
+ }
+});
 
 function Positiveness() {
+    const classes = useStyles();
+    const [value, setValue] = React.useState(0);
     return (
-        <div className="positiveness">
-      <FormControl component="fieldset">
-      <FormLabel component="legend"></FormLabel>
-      <RadioGroup aria-label="gender" name="gender1">
-        <FormControlLabel className="fontStyle" value="مثبت" control={<Radio/>} label="مثبت" />
-        <FormControlLabel value="منفی" control={<Radio />} label="منفی" />
-        <FormControlLabel value="هیچ کدام" control={<Radio />} label="هیچ کدام" />
-      </RadioGroup>
-    </FormControl>  
+        <div className="Positive">
+            <BottomNavigation
+              value={value}
+              onChange={(event, newValue) => {
+                setValue(newValue);
+              }}
+              showLabels
+              className={classes.root , "Positive_nav"}
+            >
+              <BottomNavigationAction className="bt" icon={<AddCircleIcon className="Positive_item Positive_positive"/>} />
+              <BottomNavigationAction  icon={<RadioButtonCheckedIcon className="Positive_item Positive_normal"/>} />
+              <BottomNavigationAction icon={<RemoveCircleIcon className="Positive_item Positive_negetive"/>} />
+            </BottomNavigation>
         </div>
     )
 }
