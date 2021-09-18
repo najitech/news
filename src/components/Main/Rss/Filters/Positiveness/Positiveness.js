4d042306d8670dd47481c94063894c78d7cc7719
Morteza <alirezainnaji@gmail.com>
2021-09-18 11:58:59 +0430

    change positiveness

--- src/components/Main/Rss/Filters/Positiveness/Positiveness.js
+++ src/components/Main/Rss/Filters/Positiveness/Positiveness.js
@@ -11,6 +11,13 @@ import RemoveCircleIcon from '@material-ui/icons/RemoveCircle';
 import {FaBalanceScale} from 'react-icons/fa';
 import {BsCircle} from 'react-icons/bs';
 import RadioButtonCheckedIcon from '@material-ui/icons/RadioButtonChecked';
+import FormatAlignLeftIcon from '@material-ui/icons/FormatAlignLeft';
+import FormatAlignCenterIcon from '@material-ui/icons/FormatAlignCenter';
+import FormatAlignRightIcon from '@material-ui/icons/FormatAlignRight';
+import FormatAlignJustifyIcon from '@material-ui/icons/FormatAlignJustify';
+import ToggleButton from '@material-ui/lab/ToggleButton';
+import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
+
 const useStyles = makeStyles({
   root: {
     width: 500,
@@ -26,20 +33,42 @@ const useStyles = makeStyles({
 function Positiveness() {
     const classes = useStyles();
     const [value, setValue] = React.useState(0);
+    const [alignment, setAlignment] = React.useState('left');
+    const handleAlignment = (event, newAlignment) => {
+      if (newAlignment !== null) {
+        setAlignment(newAlignment);
+      }
+    };
+  
+  
     return (
         <div className="Positive">
-            <BottomNavigation
-              value={value}
-              onChange={(event, newValue) => {
-                setValue(newValue);
-              }}
-              showLabels
-              className={classes.root , "Positive_nav"}
-            >
-              <BottomNavigationAction className="bt" icon={<AddCircleIcon className="Positive_item Positive_positive"/>} />
-              <BottomNavigationAction  icon={<RadioButtonCheckedIcon className="Positive_item Positive_normal"/>} />
-              <BottomNavigationAction icon={<RemoveCircleIcon className="Positive_item Positive_negetive"/>} />
-            </BottomNavigation>
+          <ToggleButtonGroup
+            value={alignment}
+            exclusive
+            onChange={handleAlignment}
+            aria-label="text alignment"
+            className="Positive_toggle"
+          >
+            <ToggleButton className="Positive_box" value="left" aria-label="left aligned">
+              <RemoveCircleIcon className="Remove"/>
+            </ToggleButton>
+            <ToggleButton className="Positive_box" value="center" aria-label="centered">
+             <RadioButtonCheckedIcon/>
+            </ToggleButton>
+            <ToggleButton  className="Positive_box" value="right" aria-label="right aligned">
+              <AddCircleIcon/>
+            </ToggleButton>
+          </ToggleButtonGroup>
+          {alignment=== "left" ?
+            <p className="Positive_negetive"> منفی</p> :
+            alignment==="center"?
+            <p className="Positive_none"> همه</p> :
+            alignment==="right" ?
+            <p className="Positive_Positive"> مثبت</p> :
+            null
+          }
+    
         </div>
     )
 }
