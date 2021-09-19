44acc0febffdd54e6d3fc7004ac4d67d841f6e04
alireza <alirezainnaji@gmail.com>
2021-09-19 14:25:57 +0430

    navbar added and box shadows added

---
+++ src/components/Main/Twitter/Twitter.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Twitter() {
+    return (
+        <div style={{paddingTop : 50}}>
+            <h1>This is twitter</h1>
+        </div>
+    )
+}
+
+export default Twitter
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,6 +6,7 @@ import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import './Navbar.css'
+import {Link} from 'react-router-dom'
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -19,11 +20,20 @@ const useStyles = makeStyles((theme) => ({
 function Navbar() {
     const classes = useStyles();
     return (
-        <div className={classes.root , 'Navbar'}>
+      <div className={classes.root , 'Navbar'}>
         <AppBar className="AppBar" position="static">
           <Toolbar className="AppBar" variant="dense">
-
+              <div>
+                <Link className="navLink" to="/">Home</Link>
+              </div>
+              <div>
+                <Link className="navLink" to="/RSS">RSS</Link>
+              </div>
+              <div>
+                <Link className="navLink" to="/Twitter">Twitter</Link>
+              </div>
           </Toolbar>
+          
         </AppBar>
       </div>
     )
