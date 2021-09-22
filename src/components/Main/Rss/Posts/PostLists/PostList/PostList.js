4a79d589d6c4e0a2286acddf136bc4bfb71f3f76
Morteza <alirezainnaji@gmail.com>
2021-09-22 15:00:07 +0330

    added postlist

--- src/components/Main/Rss/Posts/PostLists/PostList/PostList.js
+++ src/components/Main/Rss/Posts/PostLists/PostList/PostList.js
@@ -1,12 +1,76 @@
 import React from 'react';
-import './PostList.css'
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import SkipNextIcon from '@material-ui/icons/SkipNext';
 
-function PostList() {
-    return (
-        <div className="PostList">
-            <h2>hi</h2>
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    marginTop: '10px',
+  },
+  details: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  content: {
+    flex: '1 0 auto',
+  },
+  cover: {
+    width: '30%',
+  },
+  controls: {
+    display: 'flex',
+    alignItems: 'center',
+    paddingLeft: theme.spacing(1),
+    paddingBottom: theme.spacing(1),
+  },
+  playIcon: {
+    height: 38,
+    width: 38,
+  },
+}));
+
+export default function MediaControlCard() {
+  const classes = useStyles();
+  const theme = useTheme();
+
+  return (
+    <Card className={[classes.root , "PostList"].join(" ")}>
+
+     <CardMedia
+        className={[classes.cover , "PostList_image"].join(" ")}
+        image="https://img9.irna.ir/d/r2/2021/09/08/4/169049562.jpg"
+        title="Live from space album cover"
+      />
+
+      <div className={classes.details}>
+        <CardContent className={classes.content}>
+          <Typography component="h5" variant="h5">
+            Live From Space
+          </Typography>
+          <Typography variant="subtitle1" color="textSecondary">
+            Mac Miller
+          </Typography>
+        </CardContent>
+        <div className={classes.controls}>
+          <IconButton aria-label="previous">
+            {theme.direction === 'rtl' ? <SkipNextIcon /> : <SkipPreviousIcon />}
+          </IconButton>
+          <IconButton aria-label="play/pause">
+            <PlayArrowIcon className={classes.playIcon} />
+          </IconButton>
+          <IconButton aria-label="next">
+            {theme.direction === 'rtl' ? <SkipPreviousIcon /> : <SkipNextIcon />}
+          </IconButton>
         </div>
-    )
+      </div>
+ 
+    </Card>
+  );
 }
-
-export default PostList
