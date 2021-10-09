02e80fc75139f94cedd8862f0871b95cda38f404
alireza <alirezainnaji@gmail.com>
2021-10-09 13:43:25 +0330

    animation delay added

--- src/components/Main/Twitter/ProSearch/ProSearch.js
+++ src/components/Main/Twitter/ProSearch/ProSearch.js
@@ -1,4 +1,4 @@
-import { Button } from '@material-ui/core';
+import { Button, Zoom } from '@material-ui/core';
 import React , {useEffect , useState} from 'react';
 import { BsSearch } from 'react-icons/bs';
 import Search from '../../Rss/Search/Search';
@@ -21,15 +21,19 @@ function ProSearch(props) {
             <div className="imageContainerPS">    
                 <img height="100%" src="https://rcciworld.com/picture/bg.jpg" alt=""/>
                 <div className="imagestyle">
-                    <h1>جستجوی پیشرفته</h1>
-                    <Grow in={true}>      
+                    <Grow in={true}>    
+                        <h1>جستجوی پیشرفته</h1>
+                    </Grow>
+                    <Zoom in={true}
+                          style={{transitionDelay:'200ms',}}>      
                         <div className="toggleContainerSelect">
                             <SearchTypeTw/>
                         </div>  
-                    </Grow>
+                    </Zoom>
                     <Grow
                     in={true}
-                    style={{ transformOrigin: '0 100 0' }}
+                    style={{ transformOrigin: '0 100 0',
+                    transitionDelay:'500ms'}}
                     {...(true ? { timeout: 1000 } : {})}
                     >
                     <div className="proSearchInput" style={{}}>
@@ -40,7 +44,9 @@ function ProSearch(props) {
                     </div>
                     </Grow>
                 </div>
-                
+            </div>
+            <div className="footer">
+
             </div>
         </div>
     )
