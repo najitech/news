be40203c4b83537b9a6f89523ef5e46741eecd63
alireza <alirezainnaji@gmail.com>
2021-10-11 11:14:05 +0330

    website cards styles added

--- src/components/Main/Websites/websiteCard/WebsiteCard.js
+++ src/components/Main/Websites/websiteCard/WebsiteCard.js
@@ -1,25 +1,15 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
 import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import { red } from '@material-ui/core/colors';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
 import {FiCalendar} from 'react-icons/fi'
 import {HiOutlineLocationMarker} from 'react-icons/hi'
 import './WebsiteCard.css';
-import { CCol, CWidgetDropdown } from '@coreui/react';
-import ChartLineSimple from '../../../../views/charts/ChartLineSimple';
+import Chart from "react-apexcharts";
+
 const useStyles = makeStyles((theme) => ({
   root: {
     maxWidth: 345,
@@ -43,51 +33,62 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function WebsiteCard() {
+export default function WebsiteCard(props) {
   const classes = useStyles();
   const [expanded, setExpanded] = React.useState(false);
 
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
-
+  const [chart , setChart] = React.useState({
+    series1: [{
+    data: props.chart_data
+  }],
+  options1: {
+    chart: {
+      type: 'line',
+      width: 100,
+      height: 35,
+      sparkline: {
+        enabled: true
+      }
+    },
+  fill: {
+    colors: ['#ff891b']
+  }
+  },
+  
+});
   return (
-    <Card className={classes.root}>
+    <Card className={[classes.root , 'websiteCardRss']}>
       <div className="cardHeaderWebsite">
-            <Avatar className="avatarWebsite">R</Avatar>
-            <h4>عنوان سایت</h4>
+            <Avatar src={props.image} className="avatarWebsite">R</Avatar>
+            <h4>{props.title}</h4>
       </div>
       <CardContent>
         <Typography variant="body2" color="textSecondary" component="p">
             <div className="lastPostDateWebsite">
                 <FiCalendar style={{fontSize:17}}/>
                 &nbsp;
-                <span>25 اسفند 99</span>
+                <span>{props.publish_date}</span>
             </div>
             <div className="locationWebsiteCard">
                 <HiOutlineLocationMarker style={{fontSize:18}}/>
                 &nbsp;
-                <span>تهران</span>
+                <span>{props.location}</span>
             </div>
         </Typography>
         <div className="websiteCardFooter">
-            
-            <CWidgetDropdown
-                    className="websiteCardChart"
-                    footerSlot={
-                        <ChartLineSimple
-                        pointed
-                        className="c-chart-wrapper mt-3 mx-3"
-                        dataPoints={[65, 59, 84, 84, 51, 55, 40]}
-                        label="Members"
-                        pointHoverBackgroundColor="primary"
-                        borderColor="primary"
-                        labels="months"
-                        />
-                    }
-                    >
-                    </CWidgetDropdown>
-                    <div></div>
+        <div className="chartCardWebsite">
+          <div id="chart-1">
+            <Chart options={chart.options1}  series={chart.series1} type="bar" height={60} width={160} />
+          </div>
+        </div>
+        <div className="dataCardWebsite">
+            <span>داده در روز</span>
+            <br/>
+            {props.daily_data}
+        </div>
         </div>
       </CardContent>
     </Card>
