9625813b5b0f38a5dd50b37dca154d5fc1ad6d6e
alirezashafaee <zerocode0026@gmail.com>
2021-11-18 22:53:43 +0330

    changing some stuff

--- src/components/Main/KeywordPage/KP_Body/KP_Body_Feed/KP_BF_Search/KP_BF_Search.js
+++ src/components/Main/KeywordPage/KP_Body/KP_Body_Feed/KP_BF_Search/KP_BF_Search.js
@@ -1,18 +1,12 @@
-import { Button } from '@material-ui/core'
+import { Button, IconButton } from '@material-ui/core'
 import React from 'react'
 import { AiOutlineSearch } from 'react-icons/ai'
 import './KP_BF_Search.css'
 function KP_BF_Search(props) {
     return (
-        <div className="searchBoxKP">   
-            <div class="searchBox">
-            <input class="searchInput"type="text" name="" placeholder="Search"/>
-                <button class="searchButton" href="#">
-                    <i class="material-icons">
-                        search
-                    </i>
-                </button>
-            </div>
+        <div className="searchBoxKP">
+            <input placeholder="جستجو"/>
+            <IconButton className="searchBtnKPList"><AiOutlineSearch/></IconButton>
         </div>
     )
 }
